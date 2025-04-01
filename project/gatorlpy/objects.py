0478d83ab51c1593cea7ff5a0e981a5ee16b2a0b
Hansespinosa2 <133429105+Hansespinosa2@users.noreply.github.com>
2025-04-01 00:23:44 -0400

    Initial code skeleton for project

---
+++ project/gatorlpy/objects.py
@@ -0,0 +1,26 @@
+class Variable:
+    def __init__(self):
+        pass
+
+class Problem:
+    def __init__(self, problem_def:dict):
+        if 'minimize' in problem_def and 'maximize' not in problem_def:
+            self.objective_direction = 'minimize'
+            self.objective = problem_def['minimize']
+        elif 'maximize' in problem_def and 'minimize' not in problem_def:
+            self.objective_direction = 'maximize'
+            self.objective = problem_def['maximize']
+        else:
+            raise ValueError("The problem definition must contain either 'minimize' or 'maximize' as a key.")
+        
+        if 'subject to' in problem_def and 'constraints' not in problem_def:
+            self.constraints = problem_def['subject to']
+        elif 'constraints' in problem_def and 'subject to' not in problem_def:
+            self.constraints = problem_def['constraints']
+        else:
+            raise ValueError("The constraint definition must contain either 'subject to' or 'constraints' as a key.")
+        
+
+class Expression:
+    def __init__(self):
+        pass
---
+++ project/gatorlpy/solve.py
@@ -0,0 +1,21 @@
+import numpy as np
+from objects import Variable, Problem, Expression
+
+# Defining the way problems are made
+A = np.array([[1, 2], [4, 0], [0, 4]])  # Coefficients of the constraints
+b = np.array([8, 16, 12])  # Right-hand side of the constraints
+c = np.array([-3, -5])  # Coefficients of the objective function
+# Defining a feasible starting point
+x0 = np.zeros(len(c))  # Start with zeros
+
+
+problem_def = {
+    "minimize": [c.T @ x0],
+    "constraints": [
+        A @ x0 == b,
+        x0 >= 0
+    ]
+}
+
+problem = Problem(problem_def)
+
