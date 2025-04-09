9f6410d8f4a11b22b5959abe4da625e827df79fe
Hansespinosa2 <133429105+Hansespinosa2@users.noreply.github.com>
2025-04-09 04:25:11 -0400

    Add maximize tests

--- project/gatorlpy/tests.py
+++ project/gatorlpy/tests.py
@@ -25,7 +25,7 @@ def get_test_results(glp_prob:Problem, cvxpy_prob:cp.Problem, test_id:str):
     print(f"Test passed: {passed} \n")
 
 
-def run_test_0():
+def run_test_0a():
     # PARAMETERS
     A_arr = np.c_[np.array([[2, 1], [1, 3], [3, 1]]),np.eye(3)]
     b_arr = np.array([10,18,15])
@@ -58,7 +58,7 @@ def run_test_0():
     get_test_results(problem, problem_cvx, "1 matrix constraint, 1 var, n = 5, Slack Form, Minimize")
     
 
-def run_test_1():
+def run_test_1a():
     A_arr = np.hstack((np.array([[2, 1], [1, 3]]),np.eye(2)))
     b_arr = np.array([4, 3])
     c_arr = np.hstack((np.array([3, 2]), np.zeros(2)))
@@ -88,7 +88,102 @@ def run_test_1():
 
     get_test_results(problem, problem_cvx, "1 matrix constraint, 1 var, n = 4, Slack Form, Minimize")
 
+def run_test_0b():
+    # PARAMETERS
+    A_arr = np.c_[np.array([[2, 1], [1, 3], [3, 1]]),np.eye(3)]
+    b_arr = np.array([10,18,15])
+    c_arr = np.r_[np.array([5,4]),np.zeros(3)]
+    A = Parameter(A_arr)
+    b = Parameter(b_arr)
+    c = Parameter(c_arr)
+    zeros = Parameter(np.zeros_like(c_arr))
+    # VARIABLES
+    x = Variable(5) 
+    # PROBLEM
+    problem = Problem({
+        'maximize': c.T @ x,
+        'subject to': [
+            A @ x == b,
+            x >= zeros
+        ]
+    })
+
+    # VARIABLES
+    x_cvx = cp.Variable(5)
+    # PROBLEM
+    objective = cp.Maximize(c_arr @ x_cvx)
+    constraints = [
+        A_arr @ x_cvx == b_arr,
+        x_cvx >= 0
+    ]
+    problem_cvx = cp.Problem(objective, constraints)
+
+    get_test_results(problem, problem_cvx, "1 matrix constraint, 1 var, n = 5, Slack Form, Maximize")
+    
+
+def run_test_1b():
+    A_arr = np.hstack((np.array([[2, 1], [1, 3]]),np.eye(2)))
+    b_arr = np.array([4, 3])
+    c_arr = np.hstack((np.array([3, 2]), np.zeros(2)))
+    A = Parameter(A_arr)
+    b = Parameter(b_arr)
+    c = Parameter(c_arr)
+    zeros = Parameter(np.zeros_like(c_arr))
+    # VARIABLES
+    x = Variable(4) 
+    # PROBLEM
+    problem = Problem({
+        'maximize': c.T @ x,
+        'subject to': [
+            A @ x == b,
+            x >= zeros
+        ]
+    })
+    # VARIABLES
+    x_cvx = cp.Variable(4)
+    # PROBLEM
+    objective = cp.Maximize(c_arr @ x_cvx)
+    constraints = [
+        A_arr @ x_cvx == b_arr,
+        x_cvx >= 0
+    ]
+    problem_cvx = cp.Problem(objective, constraints)
+
+    get_test_results(problem, problem_cvx, "1 matrix constraint, 1 var, n = 4, Slack Form, Maximize")
+
 def run_test_2():
+ # PARAMETERS
+    A_arr = np.c_[-np.array([[2, 1], [1, 3], [3, 1]]),np.eye(3)]
+    b_arr = -np.array([10,18,15])
+    c_arr = np.r_[np.array([5,4]),np.zeros(3)]
+    A = Parameter(A_arr)
+    b = Parameter(b_arr)
+    c = Parameter(c_arr)
+    zeros = Parameter(np.zeros_like(c_arr))
+    # VARIABLES
+    x = Variable(5) 
+    # PROBLEM
+    problem = Problem({
+        'minimize': c.T @ x,
+        'subject to': [
+            A @ x == b,
+            x >= zeros
+        ]
+    })
+
+    # VARIABLES
+    x_cvx = cp.Variable(5)
+    # PROBLEM
+    objective = cp.Minimize(c_arr @ x_cvx)
+    constraints = [
+        A_arr @ x_cvx == b_arr,
+        x_cvx >= 0
+    ]
+    problem_cvx = cp.Problem(objective, constraints)
+
+    get_test_results(problem, problem_cvx, "1 matrix constraint, 1 var, n = 5, Slack Form, Minimize")
+
+def run_test_3():
         # PARAMETERS
     A_arr = np.c_[np.array([[2, 1], [1, 3], [3, 1]])]
     b_arr = np.array([10,18,15])
@@ -122,6 +217,9 @@ def run_test_2():
 
 
 if __name__ == "__main__":
-    # run_test_0()
-    # run_test_1()
+    run_test_0a()
+    run_test_0b()
+    run_test_1a()
+    run_test_1b()
     run_test_2()
+    run_test_3()
