7144d02eed76ec097fc8ff4706120c459ffc4587
Hansespinosa2 <133429105+Hansespinosa2@users.noreply.github.com>
2025-04-17 02:08:29 -0400

    First good draft

--- project/gatorpy/solve.py
+++ project/gatorpy/solve.py
@@ -1,24 +1,21 @@
 import numpy as np
 from lp_reductions import Variable, Problem, Parameter
 
-# Defining the way problems are made
-A = Parameter(np.array([[1, 2], [4, 0], [0, 4]]))  # Coefficients of the constraints
-b = Parameter(np.array([8, 16, 12]))  # Right-hand side of the constraints
-c = Parameter(np.array([-3, -5]))  # Coefficients of the objective function
-
-# Defining a feasible starting point
-x = Variable(len(c))  # Start with zeros
-
-
-problem_def = {
-    "minimize": c.T @ x,
-    "constraints": [
-        A @ x == b,
+### REPLACE BELOW EXAMPLE WITH YOUR PROBLEM ###
+# PARAMETERS
+A = Parameter(np.array([[2, 1], [1, 3], [3, 1]]))
+b = Parameter(np.array([10,18,15]))
+c = Parameter(np.array([5,4]))
+# VARIABLES
+x = Variable(2)
+# PROBLEM
+problem = Problem({
+    'maximize': c.T @ x,
+    'subject to': [
+        A @ x <= b,
         x >= 0
     ]
-}
+})
 
-
-problem = Problem(problem_def)
-
-problem.solve()
+solution = problem.solve()
+print(solution)
